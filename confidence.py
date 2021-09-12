f43a23c65187375aaace4b06d96ed0b482f1d2cd
Jiří Vlasák <jirivlasak9@gmail.com>
2021-09-12 22:11:12 +0200

    Added violin plots of confidence

--- confidence.py
+++ confidence.py
@@ -13,30 +13,32 @@ from scipy.stats import entropy
 
 
 # %%
+device = "cpu"
+
+# %%
 data_train = torchvision.datasets.FashionMNIST(
     "fmnist", download=True, train=True, transform=torchvision.transforms.ToTensor())
 data_loader_train = torch.utils.data.DataLoader(data_train,
-                                                batch_size=256,
+                                                batch_size=32,
                                                 shuffle=True,
                                                 )
 
 data_test = torchvision.datasets.FashionMNIST(
     "fmnist", download=True, train=False, transform=torchvision.transforms.ToTensor())
 data_loader_test = torch.utils.data.DataLoader(data_test,
-                                               batch_size=256,
+                                               batch_size=32,
                                                shuffle=False)
 
 
 # %%
 dataset_sizes = {"train": len(data_train), "val": len(data_test)}
 data_loaders = {"train": data_loader_train, "val": data_loader_test}
-
-
-# %%
-len(data_loader_train), len(data_loader_test)
-
+class_names = ["T-shirt/top", "Trouser", "Pullover", "Dress",
+               "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
 
 # %%
+
+
 class NeuralNetwork(nn.Module):
     def __init__(self, p_dropout):
         super(NeuralNetwork, self).__init__()
@@ -71,54 +73,115 @@ def mc_dropout(model, X, num_classes=10, T=100):
 def normalized_entropy(probs, num_classes=10, **kwargs):
     return entropy(probs, **kwargs)/np.log(num_classes)
 
+# %%
+
+
+class Progress:
+    def __init__(self) -> None:
+        self.predictions = np.array([])
+        self.labels = np.array([])
+        self.max_probs = np.array([])
+        self.confidences = np.array([])
+
+    def __str__(self) -> str:
+        return f"Predictions: {self.predictions}\nLabels: {self.labels}\nMax probs: {self.max_probs}\n Confidences: {self.confidences}"
+
 
 # %%
-model = NeuralNetwork(p_dropout=.5).to("cpu")
+def update_progress(progress, preds, labels, probs):
+    progress.predictions = np.append(
+        progress.predictions, preds.numpy())
+    progress.labels = np.append(
+        progress.labels, labels.data.numpy())
+    progress.max_probs = np.append(progress.max_probs, torch.max(
+        probs, dim=1)[0].detach().numpy())
+    progress.confidences = np.append(progress.confidences,
+                                     1 - normalized_entropy(probs.detach().numpy(), axis=1))
+    return progress
+
+# %%
+
+
+def train_model(model, num_epochs, optimizer, criterion):
+    softmax = nn.Softmax(dim=1)
+    precision_holder = []
+    for epoch in range(num_epochs):
+        precision_holder.append({
+            "train": Progress(),
+            "val": Progress()
+        })
+        print(f'Epoch {epoch+1}/{num_epochs}')
+        print('-' * 10)
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+            running_loss = 0.0
+            running_corrects = 0
+            running_entropy = 0.0
+            running_maxes = 0.0
+            for inputs, labels in data_loaders[phase]:
+                current_holder = precision_holder[epoch][phase]
+                optimizer.zero_grad()
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+                    probs = softmax(outputs)
+                    # statistics
+                    running_loss += loss.item() * inputs.size(0)
+                    running_corrects += torch.sum(preds == labels.data)
+                    running_entropy += np.sum(1 -
+                                              normalized_entropy(probs.detach().numpy(), axis=1))
+                    running_maxes += torch.sum(torch.max(probs, dim=1)[0])
+                    current_holder = update_progress(
+                        current_holder, preds, labels, probs)
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+            epoch_entropy = running_entropy / dataset_sizes[phase]
+            epoch_avg_max = running_maxes / dataset_sizes[phase]
+            print('{} Loss: {:.4f} Acc: {:.4f} Avg. conf: {:.4f} Avg. max. prob: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc, epoch_entropy, epoch_avg_max))
+    return precision_holder
+
+
+# %%
+model = NeuralNetwork(p_dropout=0).to(device)
 print(model)
-num_epochs = 15
 optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 criterion = nn.CrossEntropyLoss()
-softmax = nn.Softmax(dim=1)
-for epoch in range(num_epochs):
-    print('Epoch {}/{}'.format(epoch, num_epochs-1))
-    print('-' * 10)
-    for phase in ['train', 'val']:
-        if phase == 'train':
-            model.train()  # Set model to training mode
-        else:
-            model.eval()   # Set model to evaluate mode
-        running_loss = 0.0
-        running_corrects = 0
-        running_entropy = 0.0
-        running_maxes = 0.0
-        for inputs, labels in data_loaders[phase]:
-            optimizer.zero_grad()
-            # track history if only in train
-            with torch.set_grad_enabled(phase == 'train'):
-                outputs = model(inputs)
-                maxs, preds = torch.max(outputs, 1)
-                loss = criterion(outputs, labels)
-                probs = softmax(outputs)
-                # statistics
-                running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
-                running_entropy += np.sum(1 -
-                                          normalized_entropy(probs.detach().numpy(), axis=1))
-                running_maxes += torch.sum(torch.max(probs, dim=1)[0])
-                # backward + optimize only if in training phase
-                if phase == 'train':
-                    loss.backward()
-                    optimizer.step()
+progress = train_model(model, 5, optimizer, criterion)
 
-        epoch_loss = running_loss / dataset_sizes[phase]
-        epoch_acc = running_corrects.double() / dataset_sizes[phase]
-        epoch_entropy = running_entropy / dataset_sizes[phase]
-        epoch_avg_max = running_maxes / dataset_sizes[phase]
-        print('{} Loss: {:.4f} Acc: {:.4f} Avg. conf: {:.4f} Avg. max. prob: {:.4f}'.format(
-            phase, epoch_loss, epoch_acc, epoch_entropy, epoch_avg_max))
+
+# %%
+val_last_progress = progress[-1]["val"]
+correct = val_last_progress.predictions == val_last_progress.labels
+plt.figure(figsize=(15, 5))
+plt.violinplot([val_last_progress.confidences, val_last_progress.max_probs,
+                val_last_progress.confidences[correct], val_last_progress.max_probs[correct],
+                val_last_progress.confidences[~correct], val_last_progress.max_probs[~correct]])
+
+# %%
+print(
+    f"All predictions mean confidence: {np.mean(val_last_progress.confidences)}, "
+    f"Prob: {np.mean(val_last_progress.max_probs)}")
+print(
+    f"Correct predictions mean confidence: {np.mean(val_last_progress.confidences[correct])}, "
+    f"Prob: {np.mean(val_last_progress.max_probs[correct])}")
+print(
+    f"Incorrect predictions mean confidence: {np.mean(val_last_progress.confidences[~correct])}, "
+    f"Prob: {np.mean(val_last_progress.max_probs[~correct])}")
 
 # %%
 batch_size = 10
+softmax = nn.Softmax(dim=1)
+
 bin_loader = torch.utils.data.DataLoader(data_test,
                                          batch_size=batch_size,
                                          shuffle=False)
@@ -167,7 +230,7 @@ for i in range(5):
     ''')
     axs[i].imshow(x[0], cmap="gray")
     # x[0] = torch.tensor(rotate(x[0], -10))
-    x+= .2*torch.rand_like(x)
+    x += .2*torch.rand_like(x)
 
 # %%
 num_rows = 3
@@ -175,8 +238,6 @@ num_cols = 4
 inputs, classes = next(iter(data_loader_test))
 inputs = inputs[:num_rows*num_cols]
 classes = classes[:num_rows*num_cols]
-class_names = ["T-shirt/top", "Trouser", "Pullover", "Dress",
-               "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
 
 
 with torch.no_grad():
@@ -199,5 +260,3 @@ for i, (x, y) in enumerate(zip(inputs, classes)):
         r_index += 1
         c_index = 0
 plt.subplots_adjust(hspace=.4)
-
-# %%
